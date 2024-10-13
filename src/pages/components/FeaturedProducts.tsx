d3a8403c383b4c30acd1e6fffae175ec3a881a90
Nemanja0033 <antonijevicnemanja68@gmail.com>
2024-10-13 13:30:29 +0200

    feautured products update

--- src/pages/components/FeaturedProducts.tsx
+++ src/pages/components/FeaturedProducts.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import ProductCard from "./ProductCard";
 import gsap from "gsap";
+import { Button } from "@mui/material";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 const FeaturedProducts = () => {
   const featuredProductsRef = useRef<HTMLDivElement | null>(null);
@@ -25,7 +27,7 @@ const FeaturedProducts = () => {
           gsap.fromTo(
             featuredProductsRef.current,
             { opacity: 0, y: 200 },
-            { opacity: 1, y: 0, duration: 1 }
+            { opacity: 1, y: 0, duration: 1.5 }
           );
           observer.unobserve(entry.target);
         }
@@ -45,8 +47,13 @@ const FeaturedProducts = () => {
 
   return (
     <div  ref={featuredProductsRef} className='w-full flex-row bg-gray-50 mt-5'>
-      <div className="flex md:justify-start justify-center ">
-        <h1 className="md:ml-20 ml-0 mt-3 text-primary text-2xl">FEATURED</h1>
+      <div className="flex md:justify-start justify-center">
+        <h3 className="md:ml-20 ml-0 mt-3 text-primary font-semibold">F E A T U R E D</h3>
+      </div>
+
+
+      <div className="flex md:justify-start justify-center mt-3">
+        <h1 className="text-gray-700 md:ml-20 font-semibold md:text-3xl text-xl"> Discount on all interior decoration</h1>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-5 gap-5 mt-5 ml-10 mr-10">
