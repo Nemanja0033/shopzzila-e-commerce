dc52cc1221c2f256a4341189747c8f2021b9726e
nemanja <antonijevicnemanja68@gmail.com>
2024-10-25 00:44:49 +0200

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {  Github } from "lucide-react"
 const Footer = () => {
     return(
         <div className="w-full flex-row mt-[100px] text-primary">
-            <div className="w-full flex justify-center mt-3 gap-4">
+            <div className="w-full flex justify-center mb-3 gap-4">
                 <p className="">Shopzzila 2024</p>
                 <a href="https://github.com/Nemanja0033" className="flex"><Github />Nemanja Antonijevic</a>
             </div>
